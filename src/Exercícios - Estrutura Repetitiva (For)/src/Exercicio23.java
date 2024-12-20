b067f897481c4c7b55ae0f5a59237c919d3b3b3e
Eric Pfeuti <144127391+EricPfeuti@users.noreply.github.com>
2024-12-20 12:04:14 -0300

    Update Exercicio23.java

--- src/Exercícios - Estrutura Repetitiva (For)/src/Exercicio23.java
+++ src/Exercícios - Estrutura Repetitiva (For)/src/Exercicio23.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Exercicio23 {
     public static void main(String[] args) throws Exception {
-
+        // Ler um número inteiro N e calcular todos os seus divisores.
         
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i < n; i++){
+            if(n % i == 0){
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
 
     }
 }
