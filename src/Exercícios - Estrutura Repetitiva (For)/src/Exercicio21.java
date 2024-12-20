d263baefb594f12e68cd3819921751763d1d4ff3
Eric Pfeuti <144127391+EricPfeuti@users.noreply.github.com>
2024-12-20 12:03:37 -0300

    Update Exercicio21.java

--- src/Exercícios - Estrutura Repetitiva (For)/src/Exercicio21.java
+++ src/Exercícios - Estrutura Repetitiva (For)/src/Exercicio21.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Exercicio21 {
     public static void main(String[] args) throws Exception {
-
+        // Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".
         
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if(y == 0){
+                System.out.println("Divisão Impossível;");
+            } else {
+                double resultado = (double) x/y;
+                System.out.printf("%.1f;%n", resultado);
+            }
+
+        }
+
+        sc.close();
 
     }
 }
